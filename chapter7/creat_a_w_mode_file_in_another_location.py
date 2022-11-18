740cb6d01c8c049bcbdffbdf985574abfb1c4ae4
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:47:38 -0700

    Update creat_a_w_mode_file_in_another_location.py

--- chapter7/creat_a_w_mode_file_in_another_location.py
+++ chapter7/creat_a_w_mode_file_in_another_location.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: August 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 This program creates a file in a location other than the directory where the program is (like desktop)
 """
