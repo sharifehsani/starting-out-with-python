51c90a528b3387292b3e23f1fde59721a44ace14
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 13:29:51 -0700

    Update search_and_replace_methods.py

--- chapter9/search_and_replace_methods.py
+++ chapter9/search_and_replace_methods.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: September 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 # in this exercise we work a bit with the search and replace methods
 """
