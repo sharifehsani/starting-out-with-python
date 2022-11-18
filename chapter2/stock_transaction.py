75a84435cb584dd646f88d712864a12e3d5d778b
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:13:42 -0700

    Update stock_transaction.py

--- chapter2/stock_transaction.py
+++ chapter2/stock_transaction.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 10. Stock Transaction Program
 Last month Joe purchased some stock in Acme Software, Inc. Here are the details of the
