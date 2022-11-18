bb0b9a4d322cc2f1d6edf1261280ca7eca8f744a
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:12:29 -0700

    Update personal_information.py

--- chapter2/personal_information.py
+++ chapter2/personal_information.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 1. Personal Information
 Write a program that displays the following information:
