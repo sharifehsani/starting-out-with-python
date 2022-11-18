b9d8ca45517d8439624a5204c3fdedcbb6a33f37
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:27:28 -0700

    Update algorithm_workbench_4.py

--- chapter5/algorithm_workbench_4.py
+++ chapter5/algorithm_workbench_4.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: July 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 4. Write a loop that asks the user to enter a number. The loop should iterate 10 times and
 keep a running total of the numbers entered.
