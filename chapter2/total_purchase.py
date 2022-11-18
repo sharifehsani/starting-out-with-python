6728f612ef49d2a63b8c5eef41929081c4fd4016
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:14:30 -0700

    Update total_purchase.py

--- chapter2/total_purchase.py
+++ chapter2/total_purchase.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 4. Total Purchase
 A customer in a store is purchasing five items. Write a program that asks for the price of
