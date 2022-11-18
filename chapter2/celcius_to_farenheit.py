8f4b7e0b36c529d7dd11216b7068134a5d48a308
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:09:47 -0700

    Update celcius_to_farenheit.py

--- chapter2/celcius_to_farenheit.py
+++ chapter2/celcius_to_farenheit.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 9. Celsius to Fahrenheit Temperature Converter
 Write a program that converts Celsius temperatures to Fahrenheit temperatures. The formula
