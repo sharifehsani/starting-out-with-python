8a279e1a5d6dcde36d65eac300f5930b4345e5b3
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:28:45 -0700

    Update algorithm_workbench_8.py

--- chapter5/algorithm_workbench_8.py
+++ chapter5/algorithm_workbench_8.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: July 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 8. Write code that prompts the user to enter a positive nonzero number and validates the input.
 """
