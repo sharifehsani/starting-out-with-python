cf27f9ba3958dbb0fc9f2c74dcdbaf0b3f780dae
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:23:15 -0700

    Update loan_qualifier.py

--- chapter4/loan_qualifier.py
+++ chapter4/loan_qualifier.py
@@ -1,7 +1,7 @@
 """"
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 This program determines if a customer qualifies for credit card
 MIN_SALARY = 30,000
