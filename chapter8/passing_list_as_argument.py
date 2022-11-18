aeef87255cb9a0eb2fcc5915bba820ae73447cf9
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 13:12:51 -0700

    Update passing_list_as_argument.py

--- chapter8/passing_list_as_argument.py
+++ chapter8/passing_list_as_argument.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: August 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 in this program a list passed as an argument to a funciton and the functionreturns the total of the list’s elements.
 """
