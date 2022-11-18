40a3edf60575c8b99789519d1a182c5d720f9e4e
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:47:55 -0700

    Update deleting_coffee_records.py

--- chapter7/deleting_coffee_records.py
+++ chapter7/deleting_coffee_records.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: August 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 Deleting Records
 Your last task is to write a program that Julie can use to delete records from the
