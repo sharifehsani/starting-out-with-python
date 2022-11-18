c9cfa818c957d6bd39766b9b7ebf68da53ae0530
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:15:27 -0700

    Update automobil_cost.py

--- chapter3/automobil_cost.py
+++ chapter3/automobil_cost.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 4. Automobile Costs
 Write a program that asks the user to enter the monthly costs for the following expenses
