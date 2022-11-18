86fbec981c50462f3636bf14788dcecc8d62dccd
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:29:04 -0700

    Update algorithm_workbench_9.py

--- chapter5/algorithm_workbench_9.py
+++ chapter5/algorithm_workbench_9.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: July 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 9. Write code that prompts the user to enter a number in the range of 1 through 100 and validates the input.
 """
