b3e8f3b9be2a36a66b75fdd35ecc008875d3bebb
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 13:23:16 -0700

    Update checkpoint_9_15.py

--- chapter9/checkpoint_9_15.py
+++ chapter9/checkpoint_9_15.py
@@ -2,7 +2,7 @@
 """
 - Author: Sharif Ehsani
 - Date: September 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 Checkpoint
 9.15 Write a loop that asks the user “Do you want to repeat the program or quit?
