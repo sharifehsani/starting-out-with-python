1a27db1a8c1ec29a59e8a3d46570817ce92447e6
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:15:46 -0700

    Update body_mass_index.py

--- chapter3/body_mass_index.py
+++ chapter3/body_mass_index.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 6. Body Mass Index
 Write a program that calculates and displays a person’s body mass index (BMI). The
