89fdfec4738ce5992e314e1c6bf82d0cf0265528
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:33:33 -0700

    Update distance_travelled.py

--- chapter5/distance_travelled.py
+++ chapter5/distance_travelled.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: July 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 4. Distance Traveled
 The distance a vehicle travels can be calculated as follows:
