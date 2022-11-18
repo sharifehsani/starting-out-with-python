7e043486ac4141448b7d1c630a46cba8dd562e78
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:16:16 -0700

    Update calories_from_fat_and_carbohydrates.py

--- chapter3/calories_from_fat_and_carbohydrates.py
+++ chapter3/calories_from_fat_and_carbohydrates.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: June 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 7. Calories from Fat and Carbohydrates
 A nutritionist who works for a fitness club helps members by evaluating their diets. As part
