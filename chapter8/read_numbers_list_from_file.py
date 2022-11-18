35b3c56713db97c26676702e2a43c11d6a3c2f86
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 13:14:02 -0700

    Update read_numbers_list_from_file.py

--- chapter8/read_numbers_list_from_file.py
+++ chapter8/read_numbers_list_from_file.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: September 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 This program reads some random integer numbers from file and creats a list of the int numbers
 
