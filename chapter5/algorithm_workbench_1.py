856b16a9adb433cb4f53b3c83ce3cff608412cf2
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:26:46 -0700

    Update algorithm_workbench_1.py

--- chapter5/algorithm_workbench_1.py
+++ chapter5/algorithm_workbench_1.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: July 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 1. Write a while loop that lets the user enter a number. The number should be multiplied
 by 10, and the result assigned to a variable named product. The loop should iterate as
