c506682f2b9c38ef5a8517c9b4f9ad17bcafdcaf
Sharif Ehsani <47335894+sharifehsani@users.noreply.github.com>
2022-11-18 12:48:49 -0700

    Update algorithm_workbench_7.py

--- chapter7/algorithm_workbench_7.py
+++ chapter7/algorithm_workbench_7.py
@@ -1,7 +1,7 @@
 """
 - Author: Sharif Ehsani
 - Date: August 2020
-- www.gitbuh.com/ehsanis
+- https://github.com/sharifehsani
 
 Algorithm Workbench
 7. A file exists on the disk named students.txt. The file contains several records,
